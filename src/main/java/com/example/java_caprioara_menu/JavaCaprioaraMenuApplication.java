a4636ce57c5fb43e2ddcd7678bcdda7e55079e2d
Vlad-Andrei Cojocariu <vlad-andrei.cojocariu@ibm.com>
2024-07-09 19:00:25 +0300

    First commit

---
+++ src/main/java/com/example/java_caprioara_menu/JavaCaprioaraMenuApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_caprioara_menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCaprioaraMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCaprioaraMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_caprioara_menu/JavaCaprioaraMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_caprioara_menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCaprioaraMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
